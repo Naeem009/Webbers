2f8e6fe56301e733037e07670b9d0db86f1e6616
Naeem009 <56760480+Naeem009@users.noreply.github.com>
2020-07-24 22:14:50 +0500

    GitHub Logo Added

--- src/Pages/Footer.js
+++ src/Pages/Footer.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import fb from './../Images/facebookico.png';
+import gh from './../Images/githubico.png';
 
 function Footer(){
     return(
         <div>
             <footer className='ftr'>
                 <p><a href="https://web.facebook.com/Webbersofficial" target="_blank"> <img src={fb} alt="webbers" height="60px"/></a>
+                <a href="https://github.com/Naeem009" target="_blank"> <img src={gh} alt="webbers" height="56px"/></a>
                 <br/>
                     © Copyrights 2020 <span className='bar'>|</span> Alrights Reserved <span className='bar'>|</span> Webbers, Pakistan
                 <br/>
