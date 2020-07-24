12018ab1bfe695f6c7633549cc875617310b60a5
Naeem009 <56760480+Naeem009@users.noreply.github.com>
2020-07-24 21:05:49 +0500

    facebook logo added

--- src/Pages/Footer.js
+++ src/Pages/Footer.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import fb from './../Images/facebookico.png';
 
 function Footer(){
     return(
         <div>
             <footer className='ftr'>
-                <p>© Copyrights 2020 <span className='bar'>|</span> Alrights Reserved <span className='bar'>|</span> Webbers, Pakistan</p>
+                <p><a href="https://web.facebook.com/Webbersofficial" target="_blank"> <img src={fb} alt="webbers" height="60px"/></a>
+                <br/>
+                    © Copyrights 2020 <span className='bar'>|</span> Alrights Reserved <span className='bar'>|</span> Webbers, Pakistan
+                <br/>
+                </p>
             </footer>
         </div>
     );
